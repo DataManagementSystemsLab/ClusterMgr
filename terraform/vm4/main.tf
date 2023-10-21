6ac06dad0c534b9413babf90a5cd9884d86f167f
mk@hplaptopwsl <mk@hplaptopwsl>
2023-10-21 02:43:44 -0400

    vm4

--- terraform/vm4/main.tf
+++ terraform/vm4/main.tf
@@ -1,34 +1,6 @@
 # change size
 # try iso
 
-
-variable "hosts" {
-  type    = number
-  default = 1
-}
-variable "projname" {
-  type    = string
-  default = "owhpc-vm2"
-}
-variable "interface" {
-  type    = string
-  default = "ens4"
-}
-variable "memory" {
-  type    = string
-  default = "4048"
-}
-variable "vcpu" {
-  type    = number
-  default = 4
-}
-locals {
-  ips   = [for i in range(var.hosts) : "10.100.14.${format("%s", i + 10)}"]
-  names = [for i in range(var.hosts) : "owhpc-vm2-${format("%s", 10+i)}"]
-  macs  = [for i in range(var.hosts) : "ae:52:0a:b0:14:${format("%02x", i + 10)}"]
-  items = [for i in range(var.hosts) :  "${format("%s", i+10)}"]
-}
-
 terraform {
   required_providers {
     libvirt = {
@@ -46,51 +18,70 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+
+
+variable "hosts_1" {
+  type    = number
+  default = 1
+}
+
+variable "interface_1" {
+  type    = string
+  default = "ens4"
+}
+
+locals {
+  ips_1   = [for i in range(var.hosts_1) : "10.100.14.${format("%s", i + 10)}"]
+  names_1 = [for i in range(var.hosts_1) : "owhpc-vm_1-${format("%s", 10+i)}"]
+  macs_1  = [for i in range(var.hosts_1) : "ae:52:0a:b0:14:${format("%02x", i + 10)}"]
+  items_1 = [for i in range(var.hosts_1) :  "${format("%s", i+10)}"]
+}
+
 # We fetch the latest ubuntu release image from their mirrors
-resource "libvirt_volume" "vol-owhpc-vm2-qcow2" {
-  count  = var.hosts
-  name   = "owhpc-vm2-${count.index}.qcow2"
+resource "libvirt_volume" "vol-owhpc-vm_1-qcow2" {
+  count  = var.hosts_1
+  name   = "owhpc-vm_1-${count.index}.qcow2"
   pool   = "default" #CHANGE_ME
   source = "/pool/iso/ubuntu22.04.img"
   format = "qcow2"
 }
 
-resource "libvirt_cloudinit_disk" "vol-owhpc-vm2-commoninit" {
-  count = var.hosts
-  name  = "owhpc-vm2-commoninit-${local.names[count.index]}.iso"
+resource "libvirt_cloudinit_disk" "vol-owhpc-vm_1-commoninit" {
+  count = var.hosts_1
+  name  = "owhpc-vm_1-commoninit-${local.names_1[count.index]}.iso"
   pool  = "default"
   user_data = templatefile("${path.module}/cloud_init.cfg", {
-    host_name = local.names[count.index]
+    host_name = local.names_1[count.index]
     auth_key  = file("~/.ssh/id_vm.pub")
   })
   network_config = templatefile("${path.module}/network_config.cfg", {
-    interface = var.interface
-    ip_addr   = local.ips[count.index]
-    mac_addr  = local.macs[count.index]
+    interface = var.interface_1
+    ip_addr   = local.ips_1[count.index]
+    mac_addr  = local.macs_1[count.index]
   })
 }
 # Create the machine
-resource "libvirt_domain" "dom-owhpc-vm2-ubuntu" {
-  count      = var.hosts
-  name       = local.names[count.index]
+resource "libvirt_domain" "dom-owhpc-vm_1-ubuntu" {
+  count      = var.hosts_1
+  name       = local.names_1[count.index]
   memory     = "4048"
   vcpu       = 2
   machine    = "pc"
   autostart  = true
 #  qemu_agent = true # this is recommended to get ip address
   disk {
-    volume_id = libvirt_volume.vol-owhpc-vm2-qcow2[count.index].id
+    volume_id = libvirt_volume.vol-owhpc-vm_1-qcow2[count.index].id
   }
 
-  cloudinit = libvirt_cloudinit_disk.vol-owhpc-vm2-commoninit[count.index].id
+  cloudinit = libvirt_cloudinit_disk.vol-owhpc-vm_1-commoninit[count.index].id
 
   network_interface {
     network_name = "default"
   }
   network_interface {
     bridge    = "br0"
-    addresses = [local.ips[count.index]]
-    mac       = local.macs[count.index]
+    addresses = [local.ips_1[count.index]]
+    mac       = local.macs_1[count.index]
   }
   boot_device {
     dev = ["hd", "network"]
@@ -118,106 +109,78 @@ resource "libvirt_domain" "dom-owhpc-vm2-ubuntu" {
   }
 }
 
-output "ip" {
-  value = local.ips
+output "ip_1" {
+  value = local.ips_1
 }
 
-output "mac" {
-  value = local.macs
+output "mac_1" {
+  value = local.macs_1
 }
-# change size
-# try iso
 
 
-variable "hosts" {
+
+variable "hosts_2" {
   type    = number
   default = 1
 }
-variable "projname" {
-  type    = string
-  default = "owhpc-vm1"
-}
-variable "interface" {
+
+variable "interface_2" {
   type    = string
   default = "ens4"
 }
-variable "memory" {
-  type    = string
-  default = "4048"
-}
-variable "vcpu" {
-  type    = number
-  default = 4
-}
+
 locals {
-  ips   = [for i in range(var.hosts) : "10.100.15.${format("%s", i + 10)}"]
-  names = [for i in range(var.hosts) : "owhpc-vm1-${format("%s", 10+i)}"]
-  macs  = [for i in range(var.hosts) : "ae:52:0a:b0:15:${format("%02x", i + 10)}"]
-  items = [for i in range(var.hosts) :  "${format("%s", i+10)}"]
-}
-
-terraform {
-  required_providers {
-    libvirt = {
-      source = "dmacvicar/libvirt"
-    }
-  }
-
-   backend "pg" {
-    conn_str = "postgres://terraform:terra4pass@10.100.1.0/owcluster"
-   }
-}
-
-# instance the provider
-provider "libvirt" {
-  uri = "qemu:///system"
+  ips_2   = [for i in range(var.hosts_2) : "10.100.14.${format("%s", i + 10)}"]
+  names_2 = [for i in range(var.hosts_2) : "owhpc-vm_2-${format("%s", 10+i)}"]
+  macs_2  = [for i in range(var.hosts_2) : "ae:52:0a:b0:14:${format("%02x", i + 10)}"]
+  items_2 = [for i in range(var.hosts_2) :  "${format("%s", i+10)}"]
 }
 
 # We fetch the latest ubuntu release image from their mirrors
-resource "libvirt_volume" "vol-owhpc-vm1-qcow2" {
-  count  = var.hosts
-  name   = "owhpc-vm1-${count.index}.qcow2"
+resource "libvirt_volume" "vol-owhpc-vm_2-qcow2" {
+  count  = var.hosts_2
+  name   = "owhpc-vm_2-${count.index}.qcow2"
   pool   = "default" #CHANGE_ME
   source = "/pool/iso/ubuntu22.04.img"
   format = "qcow2"
 }
 
-resource "libvirt_cloudinit_disk" "vol-owhpc-vm1-commoninit" {
-  count = var.hosts
-  name  = "owhpc-vm1-commoninit-${local.names[count.index]}.iso"
+resource "libvirt_cloudinit_disk" "vol-owhpc-vm_2-commoninit" {
+  count = var.hosts_2
+  name  = "owhpc-vm_2-commoninit-${local.names_2[count.index]}.iso"
   pool  = "default"
   user_data = templatefile("${path.module}/cloud_init.cfg", {
-    host_name = local.names[count.index]
+    host_name = local.names_2[count.index]
     auth_key  = file("~/.ssh/id_vm.pub")
   })
   network_config = templatefile("${path.module}/network_config.cfg", {
-    interface = var.interface
-    ip_addr   = local.ips[count.index]
-    mac_addr  = local.macs[count.index]
+    interface = var.interface_2
+    ip_addr   = local.ips_2[count.index]
+    mac_addr  = local.macs_2[count.index]
   })
 }
 # Create the machine
-resource "libvirt_domain" "dom-owhpc-vm1-ubuntu" {
-  count      = var.hosts
-  name       = local.names[count.index]
+resource "libvirt_domain" "dom-owhpc-vm_2-ubuntu" {
+  count      = var.hosts_2
+  name       = local.names_2[count.index]
   memory     = "4048"
   vcpu       = 2
   machine    = "pc"
   autostart  = true
 #  qemu_agent = true # this is recommended to get ip address
   disk {
-    volume_id = libvirt_volume.vol-owhpc-vm1-qcow2[count.index].id
+    volume_id = libvirt_volume.vol-owhpc-vm_2-qcow2[count.index].id
   }
 
-  cloudinit = libvirt_cloudinit_disk.vol-owhpc-vm1-commoninit[count.index].id
+  cloudinit = libvirt_cloudinit_disk.vol-owhpc-vm_2-commoninit[count.index].id
 
   network_interface {
     network_name = "default"
   }
   network_interface {
     bridge    = "br0"
-    addresses = [local.ips[count.index]]
-    mac       = local.macs[count.index]
+    addresses = [local.ips_2[count.index]]
+    mac       = local.macs_2[count.index]
   }
   boot_device {
     dev = ["hd", "network"]
@@ -245,10 +208,11 @@ resource "libvirt_domain" "dom-owhpc-vm1-ubuntu" {
   }
 }
 
-output "ip" {
-  value = local.ips
+output "ip_2" {
+  value = local.ips_2
 }
 
-output "mac" {
-  value = local.macs
+output "mac_2" {
+  value = local.macs_2
 }
+
